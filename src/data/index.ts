0ab36c09d35c798e2d0c32a70535c11d2c3f0f3d
JIYEON_OnTime <54522206+Ji-Yeon0107@users.noreply.github.com>
2021-10-01 14:48:58 +0900

    Add files via upload

---
+++ src/data/index.ts
@@ -0,0 +1,17 @@
+export const imgURLObj: any = {
+    재혁: '/kjh.png',
+    예슬: '/kys.png',
+    수빈: '/psb.png',
+    지연: '/pjy.png',
+    주이: '/yjy.png',
+    정택: '/ejt.png',
+};
+
+export const defaultValueList = {
+    재혁: 0,
+    예슬: 0,
+    수빈: 0,
+    지연: 0,
+    주이: 0,
+    정택: 0,
+};
