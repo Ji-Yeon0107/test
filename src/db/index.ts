b2cba7108ae59276ebefa939ec22954ee5b35b6f
JIYEON_OnTime <54522206+Ji-Yeon0107@users.noreply.github.com>
2021-10-01 15:13:10 +0900

    Update index.ts

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,6 @@
 import faunadb from 'faunadb';
 
-const secretKey = 'fnAEIvDagzACClbH_TegqUYvdObxW1N5vGW6xm8j';
+const secretKey = 'fnAEUbPQpnAARkIDkIzk0wkuquG79V4aZ_2z34yH';
 
 const serverClient = new faunadb.Client({ secret: secretKey });
 
